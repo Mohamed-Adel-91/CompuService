67916bc3e612b58c70afef4b300b03cf1270c498
Mohamed Adel <mohamed101291@gmail.com>
2024-02-25 19:45:20 +0200

    add asyncHandler

--- backend/controllers/category.service.js
+++ backend/controllers/category.service.js
@@ -1,66 +1,61 @@
 const slugify = require("slugify");
 const CategoryModel = require("../models/category.schema");
+const asyncHandler = require("express-async-handler");
 
 // Get all categories from the database and send them to the client side
-exports.getAllCategory = async (req, res) => {
-    try {
-        const allCategories = await CategoryModel.find();
-        if (!allCategories || allCategories.length === 0) {
-            return res.status(404).json({ message: "No category found" });
-        } else {
-            return res.status(200).json(allCategories);
-        }
-    } catch (error) {
-        return res.status(500).json({ message: "Server error" });
+exports.getAllCategory = asyncHandler(async (req, res) => {
+    const allCategories = await CategoryModel.find();
+    if (!allCategories || allCategories.length === 0) {
+        return res.status(404).json({ message: "No category found" });
+    } else {
+        return res.status(200).json(allCategories);
     }
-};
+});
 
 // Add a new category in the database
-exports.createCategory = async (req, res) => {
-    try {
-        const name = req.body.name;
+exports.createCategory = asyncHandler(async (req, res) => {
+    const name = req.body.name;
+    // Checking whether the field is empty or not
+    if (!name) {
+        return res.status(400).json({ message: "Name field cannot be empty!" });
+    }
+    let category = await CategoryModel.findOne({ name: name });
+    // If there's already such a category then it will return an error otherwise it will add a new one
+    if (category) {
+        return res
+            .status(409)
+            .json({ message: "This category already exists." });
+    } else {
         const newCategory = await CategoryModel.create({
             name,
             slug: slugify(name),
         });
         res.status(201).json({ data: newCategory });
-    } catch (err) {
-        return res.status(400).json({ error: err });
     }
-};
+});
 
 // Update an existing category
-exports.updateCategory = async (req, res) => {
-    try {
-        const id = req.params.id;
-        const update = req.body;
+exports.updateCategory = asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const update = req.body;
 
-        const updatedCategory = await CategoryModel.findByIdAndUpdate(
-            id,
-            update,
-            { new: true }
-        );
+    const updatedCategory = await CategoryModel.findByIdAndUpdate(id, update, {
+        new: true,
+    });
 
-        if (!updatedCategory) {
-            return res.status(404).json({ message: "Category not found." });
-        } else {
-            return res.json(updatedCategory);
-        }
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({ message: "Server Error" });
+    if (!updatedCategory) {
+        return res.status(404).json({ message: "Category not found." });
+    } else {
+        return res.json(updatedCategory);
     }
-};
+});
 
 // Delete a category by its ID
-exports.deleteCategory = async (req, res) => {
-    try {
-        const id = req.params.id;
-        const deletedCategory = await CategoryModel.findByIdAndDelete(id);
-        return res
-            .status(200)
-            .json({ message: `${deletedCategory.name} Deleted Successfully!` });
-    } catch (err) {
-        return res.status(500).json(err);
-    }
-};
+exports.deleteCategory = asyncHandler(async (req, res) => {
+    const id = req.params.id;
+
+    const deletedCategory = await CategoryModel.findByIdAndDelete(id);
+    return res
+        .status(200)
+        .json({ message: `${deletedCategory.name} Deleted Successfully!` });
+});
