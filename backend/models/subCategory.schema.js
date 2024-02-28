f62c2344b409e18a3932690bbdad68dd77d87d06
Mohamed Adel <mohamed101291@gmail.com>
2024-02-28 18:57:48 +0200

    add subCategory schema

---
+++ backend/models/subCategory.schema.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create the Category schema
+const subCategorySchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Category Required"],
+            unique: [true, "Category must be unique"],
+            maxLength: [50, "Name cannot exceed 50 characters"],
+            minLength: [3, "Name must be at least 3 characters"],
+            trim: true,
+        },
+
+        // A and B => shopping.com/a-and-b <= this mean slug
+        slug: {
+            type: String,
+            lowercase: true,
+        },
+        images: String,
+    },
+    {
+        timestamps: true, //  Automatically create createdAt & updatedAt fields in database
+    }
+);
+
+const SubCategoryModel = mongoose.model("SubCategory", subCategorySchema);
+
+module.exports = SubCategoryModel;
